a94e3cad74405a49fd13e80489af176077f5bc15
JaredZachman <jared.zachman@gmail.com>
2025-05-09 07:07:09 -0500

    ready to deploy

--- misp2qradar.py
+++ misp2qradar.py
@@ -14,24 +14,30 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 #------*****------#
 
-misp_auth_key = "MISP-auth-key"
-qradar_auth_key = "Qradar-auth-key"
-qradar_ref_set = "MISP-RefSet-Name"
-misp_server = "MISP-Server-IP"
-qradar_server = "Qradar-Server-IP"
+misp_auth_key = ""
+qradar_auth_key = ""
+misp_server = ""
+qradar_server = ""
 frequency = 60 # In minutes
 # frequecny default is 60m.
 
 #------*****------#
 
 misp_url = "https://" + misp_server + "/attributes/restSearch/json/null/"
-QRadar_POST_url = "https://" + qradar_server + "/api/reference_data/sets/bulk_load/" + qradar_ref_set
+
 
 # EDIT TO GET OTHER DATA e.g: 
-MISP_PData = {
-    "timestamp": "1d",
-    "category": "Network activity"
-    }
+MISP_PData_list = (
+    {"qradar_ref_set": "MISP_IOC_IP", "MISP_PData": {"last": "1d", "type": "ip-src", "category": "Network activity", "enforceWarninglist": "True"}},
+    {"qradar_ref_set": "MISP_IOC_IP", "MISP_PData": {"last": "1d", "type": "ip-dst", "category": "Network activity", "enforceWarninglist": "True"}},
+    {"qradar_ref_set": "MISP_IOC_CIDR", "MISP_PData": {"last": "1d", "type": "ip-src", "category": "Network activity", "enforceWarninglist": "True"}},
+    {"qradar_ref_set": "MISP_IOC_CIDR", "MISP_PData": {"last": "1d", "type": "ip-dst", "category": "Network activity", "enforceWarninglist": "True"}},
+    {"qradar_ref_set": "MISP_IOC_MD5", "MISP_PData": {"last": "1d", "type": "md5", "category": "Payload delivery", "enforceWarninglist": "True"}},
+    {"qradar_ref_set": "MISP_IOC_SHA256", "MISP_PData": {"last": "1d", "type": "sha256", "category": "Payload delivery", "enforceWarninglist": "True"}},
+    {"qradar_ref_set": "MISP_IOC_DOMAIN", "MISP_PData": {"last": "1d", "type": "domain", "category": "Network activity", "enforceWarninglist": "True"}},
+    {"qradar_ref_set": "MISP_IOC_URL", "MISP_PData": {"last": "1d", "type": "url", "category": "Network activity", "enforceWarninglist": "True"}}
+)
+
 
 # MISP_PData = {
 #    "timestamp": "1d",
@@ -53,25 +59,31 @@ QRadar_headers = {
     }
 
 def validate_refSet():
-    validate_refSet_url = "https://" + qradar_server + "/api/reference_data/sets/" + qradar_ref_set
-    validate_response = requests.request("GET", validate_refSet_url, headers=QRadar_headers, verify=False)
-    print (time.strftime("%H:%M:%S") + " -- " + "Validating if reference set " + qradar_ref_set + " exists")
-    if validate_response.status_code == 200:
-        print(time.strftime("%H:%M:%S") + " -- " + "Validating reference set " + qradar_ref_set + " - (Success) ")
-        validate_response_data = validate_response.json()
-        refSet_etype = (validate_response_data["element_type"])
-        print(time.strftime("%H:%M:%S") + " -- " + "Identifying Reference set " + qradar_ref_set + " element type")
-        print(time.strftime("%H:%M:%S") + " -- " + "Reference set element type = " + refSet_etype + " (Success) ")
-        if refSet_etype == "IP":
-            print (time.strftime("%H:%M:%S") + " -- " + "The QRadar Reference Set " + qradar_ref_set + " Element Type = \"IP\". Only IPs will be imported to QRadar and the other IOC types will be discarded")
-            get_misp_data(refSet_etype)
+    for import_export_pair in MISP_PData_list:
+        qradar_ref_set = import_export_pair["qradar_ref_set"]
+        MISP_PData = import_export_pair["MISP_PData"]
+        validate_refSet_url = "https://" + qradar_server + "/api/reference_data/sets/" + qradar_ref_set
+        validate_response = requests.request("GET", validate_refSet_url, headers=QRadar_headers, verify=False)
+        print (time.strftime("%H:%M:%S") + " -- " + "Validating if reference set " + qradar_ref_set + " exists")
+        if validate_response.status_code == 200:
+            print(time.strftime("%H:%M:%S") + " -- " + "Validating reference set " + qradar_ref_set + " - (Success) ")
+            validate_response_data = validate_response.json()
+            refSet_etype = (validate_response_data["element_type"])
+            print(time.strftime("%H:%M:%S") + " -- " + "Identifying Reference set " + qradar_ref_set + " element type")
+            print(time.strftime("%H:%M:%S") + " -- " + "Reference set element type = " + refSet_etype + " (Success) ")
+            if refSet_etype == "IP" or refSet_etype == "CIDR":
+                print (time.strftime("%H:%M:%S") + " -- " + "The QRadar Reference Set " + qradar_ref_set + " Element Type = \"IP\". Only IPs will be imported to QRadar and the other IOC types will be discarded")
+                get_misp_data(refSet_etype, qradar_ref_set, MISP_PData)
+            else:
+                get_misp_data(refSet_etype, qradar_ref_set, MISP_PData)
         else:
-            get_misp_data(refSet_etype)
-    else:
-        print(time.strftime("%H:%M:%S") + " -- " + "QRadar Reference Set does not exist, please verify if reference set exists in QRadar.")
-        sys.exit()
+            print(time.strftime("%H:%M:%S") + " -- " + "QRadar Reference Set does not exist, please verify if reference set exists in QRadar.")
+            sys.exit()
 
-def get_misp_data(refSet_etype):
+
+
+def get_misp_data(refSet_etype, qradar_ref_set, MISP_PData):
+    QRadar_POST_url = "https://" + qradar_server + "/api/reference_data/sets/bulk_load/" + qradar_ref_set
     print(time.strftime("%H:%M:%S") + " -- " + "Initiating, GET data from MISP on " + misp_server)
     misp_response = requests.request('POST', misp_url, json=MISP_PData, headers=MISP_headers, verify=False)
     json_data = misp_response.json()
@@ -79,36 +91,50 @@ def get_misp_data(refSet_etype):
     if misp_response.status_code == 200:
         print(time.strftime("%H:%M:%S") + " -- " + "MISP API Query (Success) ")
         for data in json_data["response"]["Attribute"]:
-            iocs = (data['value'])
-            ioc_list.append(iocs)
+            ioc = (data['value'])
+            if refSet_etype == "CIDR":
+                if "/" not in ioc:
+                    continue
+            if refSet_etype == "IP":
+                if "/" in ioc:
+                    continue
+            if "|" in ioc:
+                pipe_values = ioc.split("|")
+                for value in pipe_values:
+                    ioc_list.append(value)
+                    print(f"split {pipe_values} to {value}")
+                continue
+            ioc_list.append(ioc)
         import_data = json.dumps(ioc_list)
         ioc_count = len(ioc_list)
         print(time.strftime("%H:%M:%S") + " -- " + str(ioc_count) + " IOCs imported")
         if refSet_etype == "IP":
             print(time.strftime("%H:%M:%S") + " -- " + "Trying to clean the IOCs to IP address, as " + qradar_ref_set + " element type = IP")
-            r = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+            r = re.compile(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
             ioc_cleaned = list(filter(r.match, ioc_list))
             ioc_cleaned_data = json.dumps(ioc_cleaned)
             ioc_count_cleaned = len(ioc_cleaned)
             print(time.strftime("%H:%M:%S") + " -- " + "(Success) Extracted " + str(ioc_count_cleaned) + " IPs from initial import.")
-            qradar_post_IP(ioc_cleaned_data, ioc_count_cleaned)
+            qradar_post_IP(ioc_cleaned_data, ioc_count_cleaned, qradar_ref_set)
         else:
-            qradar_post_all(import_data, ioc_count)
+            qradar_post_all(import_data, ioc_count, qradar_ref_set)
     else:
         print(time.strftime("%H:%M:%S") + " -- " + "MISP API Query (Failed), Please check the network connectivity")
         sys.exit()
 
-def qradar_post_IP(ioc_cleaned_data, ioc_count_cleaned):
+def qradar_post_IP(ioc_cleaned_data, ioc_count_cleaned, qradar_ref_set):
+    QRadar_POST_url = "https://" + qradar_server + "/api/reference_data/sets/bulk_load/" + qradar_ref_set
     print(time.strftime("%H:%M:%S") + " -- " + "Initiating, IOC POST to QRadar ")
     qradar_response = requests.request("POST", QRadar_POST_url, data=ioc_cleaned_data, headers=QRadar_headers, verify=False)
     if qradar_response.status_code == 200:
         print(time.strftime("%H:%M:%S") + " -- " + "Imported " + str(ioc_count_cleaned) + " IOCs to QRadar (Success)" )
         print(time.strftime("%H:%M:%S") + " -- " + "Waiting to next schedule in " + str(frequency) + "minutes")
     else:
-        print(time.strftime("%H:%M:%S") + " -- " + "Could not POST IOCs to QRadar (Failure)")   
+        print(time.strftime("%H:%M:%S") + " -- " + "Could not POST IOCs to QRadar (Failure)")
       
 
-def qradar_post_all(import_data, ioc_count):
+def qradar_post_all(import_data, ioc_count, qradar_ref_set):
+    QRadar_POST_url = "https://" + qradar_server + "/api/reference_data/sets/bulk_load/" + qradar_ref_set
     print(time.strftime("%H:%M:%S") + " -- " + "Initiating, IOC POST to QRadar ")
     qradar_response = requests.request("POST", QRadar_POST_url, data=import_data, headers=QRadar_headers, verify=False)
     if qradar_response.status_code == 200:
@@ -137,6 +163,10 @@ def socket_check_misp():
     else:
         print(time.strftime("%H:%M:%S") + " -- " + "Could not establish HTTPS connection to MISP Server, Please check connectivity before proceeding.")
 
-scheduler = BlockingScheduler()
-scheduler.add_job(socket_check_qradar, 'interval', minutes=frequency, next_run_time=datetime.datetime.now())
-scheduler.start()
+# scheduler = BlockingScheduler()
+# scheduler.add_job(socket_check_qradar, 'interval', minutes=frequency, next_run_time=datetime.datetime.now())
+# scheduler.start()
+
+
+if __name__ == "__main__":
+    validate_refSet()
